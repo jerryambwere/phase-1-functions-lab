a0243cc7eff1a352c172aad581b136507c2f99d4
Melvin jerry <+>
2024-06-26 16:02:37 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+let someValue;
+function distanceFromHqInBlocks(someValue) {
+  if (someValue > 42) {
+    return someValue - 42;
+  }
+
+  return 42 - someValue;
+}
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(someValue) {
+  distanceFromHqInBlocks(someValue);
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+distanceFromHqInFeet();
+
+function distanceTravelledInFeet(start, destination) {
+  if (start > destination) {
+    return (start - destination) * 264;
+  }
+  return (destination - start) * 264;
+}
+
+distanceTravelledInFeet();
+
+function calculatesFarePrice(start, destination) {
+  if ((destination - start) * 264 <= 400) {
+    return 0
+  }
+    if ((destination - start) * 264)>400 {
+        return "destination - start) * 264"
+
+  }
+
+
+  }
+
+
+calculateFarePrice();
